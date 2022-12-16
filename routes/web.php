ac942356fc605abc03ab48513a866bec23258d47
Luke Sheppard <luke@luke-sheppard.com>
2022-12-16 15:14:44 +0000

    Move sales route handling to a controller

--- routes/web.php
+++ routes/web.php
@@ -19,12 +19,8 @@ Route::get('/', function () {
 
 Route::redirect('/dashboard', '/sales');
 
-Route::get('/sales', function () {
-    return view('coffee_sales');
-})->middleware(['auth'])->name('coffee.sales');
-
-Route::get('/shipping-partners', function () {
-    return view('shipping_partners');
-})->middleware(['auth'])->name('shipping.partners');
+Route::get('/sales', [\App\Http\Controllers\SalesController::class, 'index'])
+    ->middleware(['auth'])
+    ->name('coffee.sales');
 
 require __DIR__.'/auth.php';
